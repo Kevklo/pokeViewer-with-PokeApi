912508bf88aa52f3e9bd532410c2a30ee9b97210
Kunklo <126540566+Kunklo@users.noreply.github.com>
2024-10-29 21:10:04 -0300

    fixing isue with gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pokeViewer-with-PokeApi/"
-})
+  base: "/pokeViewer-with-PokeApi/",
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
+  },
+});
