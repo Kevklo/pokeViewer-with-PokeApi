8e98d0a5a958b6340f1f10d7a16450f11ebee177
Kunklo <126540566+Kunklo@users.noreply.github.com>
2024-10-29 22:23:36 -0300

    fixing isue with gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://kevklo.github.io/pokeViewer-with-PokeApi",
   build: {
     rollupOptions: {
       external: [],
